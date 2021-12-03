4d9fb21b7405bde67b0c9d878d3020a034f2e0a1
abagogogo <rcnjko@yahoo.com.tw>
2021-12-04 07:41:58 +0800

    Use Math.MinInt32 instead of hard coding

--- array.go
+++ array.go
@@ -7,7 +7,7 @@ func reverseArray(a []int32) []int32 {
 
 func hourglassSum(arr [][]int32) int32 {
     var sum, maxSum int32
-    maxSum = -999999    // FIXIT: minimum of int32
+    maxSum = math.MinInt32
     for i := 0; i < len(arr) - 2; i = i + 1 {
         for j := 0; j < len(arr[i]) - 2; j = j + 1 {
             sum = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]

e69912085bbe70aa5adc5ee1146b45a23b6db096
abagogogo <rcnjko@yahoo.com.tw>
2021-12-03 08:18:15 +0800

    2D array manimuplation

--- array.go
+++ array.go
@@ -4,3 +4,17 @@ func reverseArray(a []int32) []int32 {
     }
     return a
 }
+
+func hourglassSum(arr [][]int32) int32 {
+    var sum, maxSum int32
+    maxSum = -999999    // FIXIT: minimum of int32
+    for i := 0; i < len(arr) - 2; i = i + 1 {
+        for j := 0; j < len(arr[i]) - 2; j = j + 1 {
+            sum = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
+            if maxSum < sum {
+                maxSum = sum
+            }
+        }
+    }
+    return maxSum
+}
